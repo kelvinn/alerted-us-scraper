02ce8365e7f71485ddc99c092e8d3d975a0612bc
Kelvin N <kelvin@kelvinism.com>
2015-02-16 22:16:12 +1100

    Converting result as string

--- common.py
+++ common.py
@@ -54,7 +54,7 @@ def transmit(alerts):
             if resp.status_code == 201:
                 result = True
 
-            transmit_to_keen("transmit", {"result": "%s" % result, "sender": "%s" % name,
+            transmit_to_keen("transmit", {"result": "%s" % str(result), "sender": "%s" % name,
                                           "status_code": "%s" % status_code, "identifier": "%s" % identifier})
     return result
 
