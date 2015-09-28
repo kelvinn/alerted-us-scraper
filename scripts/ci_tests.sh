3f99f6dd14579e1a202888f5a87bd0b834030079
Kelvin N <kelvin@kelvinism.com>
2015-09-28 23:28:57 +1300

    Removing shall sums

--- scripts/ci_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo docker build -t alerted-us-scraper .
-sudo docker run alerted-us-scraper python tests.py
-sudo docker login -e $TUTUM_EMAIL -u $TUTUM_USER -p $TUTUM_PASS tutum.co
-sudo docker tag alerted-us-scraper tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
-sudo docker push tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo pip install -U tutum requests
-tutum service set --image tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_PROD_NAME --sync
-tutum service redeploy $TUTUM_PROD_NAME --sync
