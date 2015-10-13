29dd8145ea5e693417eff7187ae8ad1d18eb051c
Kelvin N <kelvin@kelvinism.com>
2015-10-13 14:25:57 +1300

    Specify needed version of requests

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-pip install -U tutum requests loaderio
+pip install -U tutum requests==2.7.0 loaderio
 tutum service set --image tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
 tutum service redeploy $TUTUM_ENVIRONMENT_NAME --sync
