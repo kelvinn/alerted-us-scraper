0859257d17354c83c2e8ef6651186351eccd405e
Kelvin N <kelvin@kelvinism.com>
2015-10-13 14:20:02 +1300

    Change docker tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 sudo docker build -t alerted-us-scraper .
 sudo docker run alerted-us-scraper python tests.py
 sudo docker login -e $TUTUM_EMAIL -u $TUTUM_USER -p $TUTUM_PASS tutum.co
-sudo docker tag repo_web tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
+sudo docker tag alerted-us-scraper tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
 sudo docker push tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
