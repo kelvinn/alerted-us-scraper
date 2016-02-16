003c8059b2c5a4d7cafd726cad64bcba41f34eda
Kelvin N <kelvin@kelvinism.com>
2016-02-16 13:56:09 +1300

    Tutum/Snap -> DockerHub/Semaphore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+# Abort the script if any command fails
+set -e
 
-pip install -U tutum requests==2.7.0 loaderio
-tutum service set --image tutum.co/zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
-tutum service redeploy $TUTUM_ENVIRONMENT_NAME --sync
+# Call this like deploy.sh some-name-on-docker-cloud https://some-name.com
+
+TARGET_ENVNAME=$1
+TARGET_HOSTNAME=$2
+
+virtualenv ~/venv
+source ~/venv/bin/activate
+
+pip install -U docker-cloud requests==2.7.0
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker-cloud service set --image zephell/alerted-us-scraper:$SEMAPHORE_BUILD_NUMBER-$SEMAPHORE_BRANCH_ID $TARGET_ENVNAME --sync
+docker-cloud service redeploy $TARGET_ENVNAME --sync
--- scripts/push.sh
+++ scripts/push.sh
@@ -3,6 +3,6 @@
 # Abort the script if any command fails
 set -e
 
-docker login -e $TUTUM_EMAIL -u $TUTUM_USER -p $TUTUM_PASS tutum.co
-docker tag alerted-us-scraper tutum.co/zephell/alerted-us-scraper:$SEMAPHORE_BUILD_NUMBER-$SEMAPHORE_BRANCH_ID
-docker push tutum.co/zephell/alerted-us-scraper:$SEMAPHORE_BUILD_NUMBER-$SEMAPHORE_BRANCH_ID
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker tag alerted-us-scraper zephell/alerted-us-scraper:$SEMAPHORE_BUILD_NUMBER-$SEMAPHORE_BRANCH_ID
+docker push zephell/alerted-us-scraper:$SEMAPHORE_BUILD_NUMBER-$SEMAPHORE_BRANCH_ID
