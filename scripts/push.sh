a286c947b3d484f3592bea2d6a94bff12103fa61
Kelvin N <kelvin@kelvinism.com>
2016-05-24 21:49:19 +0200

    Adjust how image is tagged

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker tag alerted-us-scraper zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
-docker push zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
+docker tag alerted-us-scraper zephell/alerted-us-scraper:$SNAP_PIPELINE_COUNTER-$SNAP_COMMIT_SHORT
+docker push zephell/alerted-us-scraper:$SNAP_PIPELINE_COUNTER-$SNAP_COMMIT_SHORT
