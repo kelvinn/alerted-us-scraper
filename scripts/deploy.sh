b8825ddd4bc9a1af845551ed5c921e7490dcf582
Kelvin N <kelvin@kelvinism.com>
2016-05-24 21:47:27 +0200

    Update Deploy script for SnapCI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,5 +13,5 @@ source ~/venv/bin/activate
 pip install -U docker-cloud requests==2.7.0
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker-cloud service set --image zephell/alerted-us-scraper:$SEMAPHORE_BUILD_NUMBER-$SEMAPHORE_BRANCH_ID $TARGET_ENVNAME --sync
+docker-cloud service set --image zephell/alerted-us-scraper:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TARGET_ENVNAME --sync
 docker-cloud service redeploy $TARGET_ENVNAME --sync
