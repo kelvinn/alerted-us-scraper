aaab0f52cadf61985942977b0931f16519baae00
Kelvin Nicholson <kelvin@kelvinism.com>
2017-03-20 23:30:47 +1100

    Typo. Space added.

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker tag alerted-us-scraperzephell/alerted-us-scraper:1.1.$SEMAPHORE_BUILD_NUMBER
+docker tag alerted-us-scraper zephell/alerted-us-scraper:1.1.$SEMAPHORE_BUILD_NUMBER
 docker push zephell/alerted-us-scraper:1.1.$SEMAPHORE_BUILD_NUMBER
