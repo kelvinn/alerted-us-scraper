97db15e32ae60ca702efad9c48f755a3bcfcad34
Kelvin Nicholson <kelvin@kelvinism.com>
2017-03-20 23:16:07 +1100

    Migrating to SemaphoreCI. Updated push scripts with correct env vars.

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker tag alerted-us-scraper zephell/alerted-us-scraper:1.0.$SNAP_PIPELINE_COUNTER
-docker push zephell/alerted-us-scraper:1.0.$SNAP_PIPELINE_COUNTER
+docker tag alerted-us-scraperzephell/alerted-us-scraper:1.1.$SEMAPHORE_BUILD_NUMBER
+docker push zephell/alerted-us-scraper:1.1.$SEMAPHORE_BUILD_NUMBER
