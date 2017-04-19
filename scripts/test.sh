e791c338d1c495c2df4c69417f534baea079a724
Kelvin Nicholson <kelvin@kelvinism.com>
2017-04-19 22:41:47 +1000

    Install libs

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,9 @@
 # Abort the script if any command fails
 set -e
 
+# Install vendor libraries
+pip install -r requirements.txt --upgrade -t lib
+
 # Run the tests
 python runner.py ~/bin/google-cloud-sdk/ --test-path .
 
