136b4944884cee2504785b5b773d984f992e01ee
Kelvin Nicholson <kelvin@kelvinism.com>
2017-04-19 23:01:25 +1000

    Add lib files a second time

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 # Abort the script if any command fails
 set -e
 
+# Install vendor libraries
+pip install -r requirements.txt --upgrade -t lib
+
 # Login to Google App Engine (need to enable API Admin)
 ~/bin/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=/home/runner/.config/gcloud/application_default_credentials.json
 
