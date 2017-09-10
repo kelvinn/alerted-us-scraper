7d5233186a95aac00f1d744d32d44634222f090f
Kelvin Nicholson <kelvin@kelvinism.com>
2017-09-10 22:33:49 +1000

    Docker CLI no longer uses or requires -e ?

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,6 +3,6 @@
 # Abort the script if any command fails
 set -e
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker tag alerted-us-scraper zephell/alerted-us-scraper:1.1.$SEMAPHORE_BUILD_NUMBER
 docker push zephell/alerted-us-scraper:1.1.$SEMAPHORE_BUILD_NUMBER
