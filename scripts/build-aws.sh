52317d9f4fe9ad212aa7dc5ae3f2aeacb1a756fa
Kelvin Nicholson <kelvin@kelvinism.com>
2018-04-23 13:48:31 +1000

    Don't double build

--- scripts/build-aws.sh
+++ scripts/build-aws.sh
@@ -3,7 +3,5 @@
 # Abort the script if any command fails
 set -e
 
-docker run -v `pwd`:/code -it amazonlinux bash -c "yum -y install python27-pip gcc python27-devel && pip install -t /code/vendored/ -r /code/requirements.txt"
-
 docker build -t alerted-us-scraper .
 docker run alerted-us-scraper python tests.py
