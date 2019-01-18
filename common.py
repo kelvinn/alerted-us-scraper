26cf5fa8ba42df862e418e05bc70b1eeae10d72b
Kelvin Nicholson <kelvin@kelvinism.com>
2019-01-18 23:51:34 +1100

    Remove xray

--- common.py
+++ common.py
@@ -1,18 +1,9 @@
 import logging
-import os
-import sys
 from os import getenv
 import base64
-
-# get this file's directory independent of where it's run from
-here = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(here, "vendored"))
-
 import requests
 from capparselib.parsers import CAPParser
 from dogpile.cache import make_region
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch
 
 
 ALERTED_USERPASS = getenv('ALERTED_USERPASS', 'admin:password')
@@ -55,7 +46,7 @@ def get_cache():
     return region
 
 
-def transmit(alerts, trace_entity=None):
+def transmit(alerts):
     """
     A function to transmit XML to Alerted web service
 
@@ -65,9 +56,6 @@ def transmit(alerts, trace_entity=None):
     #
     # Determine if the alert can be parsed as valid CAP XML
     # This will be erased on each deploy to Heroku, but that's OK
-
-    # Set the parent X-Ray entity for the worker thread.
-    xray_recorder.set_trace_entity(trace_entity)
 
     cache = get_cache()
     result = False
@@ -99,6 +87,4 @@ def transmit(alerts, trace_entity=None):
             else:
                 print("Unable to submit alert (%s) %s" % (str(resp.status_code), identifier))
 
-    # prevent thread pollution
-    xray_recorder.clear_trace_entities()
     return result
--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-import sys
 import logging
-import os
 import json
 from datetime import datetime
+from spiders import rfs, usgs, taiwan, noaa
+from common import transmit
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-# get this file's directory independent of where it's run from
-here = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(here, "vendored"))
-
-from aws_xray_sdk.core import xray_recorder
-
-from spiders import rfs, usgs, taiwan, noaa
-from common import transmit
-
-
-# Get the current active segment or subsegment from the main thread.
-current_entity = xray_recorder.get_trace_entity()
 functions = [rfs, usgs, taiwan, noaa]
 
 
@@ -26,7 +14,7 @@ def app():
     print("Running spiders at %s" % datetime.now())
     for fn in functions:
         alerts = fn()
-        transmit(alerts, current_entity)
+        transmit(alerts)
 
 
 def handler(event, context):
@@ -38,7 +26,7 @@ def handler(event, context):
         results = []
         for fn in functions:
             alerts = fn()
-            results.append(transmit(alerts, current_entity))
+            results.append(transmit(alerts))
 
         response['result'] = results
     except Exception as e:
