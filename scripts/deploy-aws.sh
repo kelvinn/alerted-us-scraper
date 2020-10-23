f219ea6d603fffa472249dbc2475ac763b587f97
Kelvin Nicholson <kelvin@kelvinism.com>
2020-10-23 23:06:07 +1100

    Get CI working

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -3,6 +3,7 @@
 # Abort the script if any command fails
 set -e
 
+source .venv/bin/activate
 sudo npm install -g serverless
 sls plugin install -n serverless-python-requirements
 sls deploy
--- scripts/test-aws.sh
+++ scripts/test-aws.sh
@@ -3,4 +3,5 @@
 # Abort the script if any command fails
 set -e
 
+source .venv/bin/activate
 python tests.py
