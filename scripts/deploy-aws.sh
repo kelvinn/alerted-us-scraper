92d97d7b732f6ffb872bcf8df1cb7d320e15d9b5
Kelvin Nicholson <kelvin@kelvinism.com>
2021-07-09 11:20:43 +1000

    Use env variable

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -7,5 +7,5 @@ python3 -m venv .venv
 source .venv/bin/activate
 npm install -g serverless
 sls plugin install -n serverless-python-requirements
-sls config credentials --provider aws --key ${{ secrets.AWS_ACCESS_KEY_ID }} --secret ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+sls config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 sls deploy
